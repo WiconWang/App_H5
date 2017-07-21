c2b9db858f9d497aae0038d0541fe1f06edc638f
Wicon Wang <wiconwang@gmail.com>
2017-07-21 12:04:34 +0800

    Added:添加模块

---
+++ app/src/main/java/wang/wicon/micrositeapp/MainActivity.java
@@ -0,0 +1,125 @@
+package wang.wicon.micrositeapp;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //全屏
+
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        WebView webView = (WebView) findViewById(R.id.webView);
+        super.onDestroy();
+        webView.removeAllViews();
+        webView.destroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView webView = (WebView) findViewById(R.id.webView);
+        if(keyCode == KeyEvent.KEYCODE_BACK&&webView.canGoBack()){
+            webView.goBack();//返回上个页面
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);//退出整个应用程序
+    }
+
+    private void init(){
+        WebView webView = (WebView) findViewById(R.id.webView);
+        //WebView加载web资源
+        webView.loadUrl("http://www.wangweiqiang.com/");
+        //启用支持javascript
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setUseWideViewPort(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setLoadWithOverviewMode(true);
+
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+
+                if(url.contains("alipays://platformapi") || url.contains("wechat://") || url.contains("weixin://")  || url.contains("mttbrowser://")  || url.contains("mqqwpa://") || url.contains("mqqapi://")  || url.contains("sinaweibo://")){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                } else {
+                    view.loadUrl(url);
+                }
+
+                //开启JS
+                view.getSettings().setJavaScriptEnabled(true);
+                view.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                return true;
+            }
+
+        });
+
+        // 开启JS的弹窗等组件
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+
+                AlertDialog.Builder b2 = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("提示").setMessage(message)
+                        .setPositiveButton("ok",
+                                new AlertDialog.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        // TODO Auto-generated method stub
+                                        result.confirm();
+                                    }
+                                });
+
+                b2.setCancelable(false);
+                b2.create();
+                b2.show();
+                return true;
+            }
+
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+                // TODO Auto-generated method stub
+                return super.onJsConfirm(view, url, message, result);
+            }
+
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue,
+                                      JsPromptResult result) {
+                // TODO Auto-generated method stub
+                return super.onJsPrompt(view, url, message, defaultValue, result);
+            }
+        });
+    }
+
+
+}
